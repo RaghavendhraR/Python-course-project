3ffc3b22567271827ef5542ce57014f64a098569
RaghavendhraR <raghulramesh843@gmail.com>
2021-08-04 09:54:02 +0530

    changig a name

--- script/myscript.js
+++ script/myscript.js
@@ -6,3 +6,22 @@ Image.onclick = function () {
     Image.setAttribute("src", "../jackie.jpg");
   else Image.setAttribute("src", "../brucelee.jpg");
 };
+
+var nameButton = document.querySelector("button");
+var nameHeading = document.querySelector("h2");
+function setUserName() {
+  "use strict";
+  var myName = window.prompt("please enter your name.");
+  localStorage.setItem("name", myName);
+  nameHeading.textContent = "TODAY IS A NEW DAY," + myName;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  nameHeading.textContent = "TODAY IS A NEW DAY," + storedName;
+}
+nameButton.onclick = function () {
+  "use strict";
+  setUserName();
+};
